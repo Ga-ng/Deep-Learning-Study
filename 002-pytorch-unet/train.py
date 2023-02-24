4ada11f84e50f5bc17c783368962cd35edfb1711
Ga-ng <kounghun0328@gmail.com>
2023-02-24 21:54:58 +0900

    UNet: Implementing

---
+++ 002-pytorch-unet/train.py
@@ -0,0 +1,167 @@
+## 라이브러리 추가하기
+import os
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from torchvision import transforms, datasets
+
+## training 에 필요한 하이퍼파라미터 설정하기
+lr = 1e-3
+batch_size = 4
+num_epoch = 100
+
+# 데이터가 저장되어 있는
+data_dir = './datasets'
+
+# train 될 네트워크가 저장될
+chkpt_dir = './checkpoint'
+
+# 텐서보드 로그파일이 저장될
+log_dir = './log'
+
+# train이 GPU, CPU 어디서 동작할지? 결정하는 device flag
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+## UNet Network 구현
+class UNet(nn.Module):
+    def __init__(self):
+        super(UNet, self).__init__()
+
+        # 반복적으로 나오는 레이어를 함수로 설정
+        def CBR2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True):
+            layers = []
+
+            # 우선 Convolution layer에 대한 정의
+            layers += [nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                                 kernel_size=kernel_size, stride=stride, padding=padding,
+                                 bias=bias)]
+
+            # 다음은 batch normalization layer에 대한 정의
+            layers += [nn.BatchNorm2d(num_features=out_channels)]
+
+            # RELU 정의
+            layers += [nn.ReLU()]
+
+            cbr = nn.Sequential(*layers)
+
+            return cbr
+
+        ## Contracting path
+        # Naming -> encoder, stage: 1, CBR: 몇번째인지
+        # 첫번째 stage, 각 함수는 파란색 화살표
+        self.enc1_1 = CBR2d(in_channels=1, out_channels=64)
+        self.enc1_2 = CBR2d(in_channels=64, out_channels=64)
+
+        # 빨간색 화살표
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+
+        # stage: 2
+        self.enc2_1 = CBR2d(in_channels=64, out_channels=128)
+        self.enc2_2 = CBR2d(in_channels=128, out_channels=128)
+
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc3_1 = CBR2d(in_channels=128, out_channels=256)
+        self.enc3_2 = CBR2d(in_channels=256, out_channels=256)
+
+        self.pool3 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc4_1 = CBR2d(in_channels=256, out_channels=512)
+        self.enc4_2 = CBR2d(in_channels=512, out_channels=512)
+
+        self.pool4 = nn.MaxPool2d(kernel_size=2)
+
+        # 맨 아래 마지막 레이어 추가로 encoder 파트 종료
+        self.enc5_1 = CBR2d(in_channels=512, out_channels=1024)
+
+        ## Expansive path - 오른쪽
+        self.dec5_1 = CBR2d(in_channels=1024, out_channels=512)
+
+        self.unpool4 = nn.ConvTranspose2d(in_channels=512, out_channels=512,
+                                          kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec4_2 = CBR2d(in_channels=2*512, out_channels=512)
+        self.dec4_1 = CBR2d(in_channels=512, out_channels=256)
+
+        self.unpool3 = nn.ConvTranspose2d(in_channels=256, out_channels=256,
+                                          kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec3_2 = CBR2d(in_channels=2*256, out_channels=256)
+        self.dec3_1 = CBR2d(in_channels=256, out_channels=128)
+
+        self.unpool2 = nn.ConvTranspose2d(in_channels=128, out_channels=128,
+                                          kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec2_2 = CBR2d(in_channels=2*128, out_channels=128)
+        self.dec2_1 = CBR2d(in_channels=128, out_channels=64)
+
+        self.unpool1 = nn.ConvTranspose2d(in_channels=64, out_channels=64,
+                                          kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec1_2 = CBR2d(in_channels=2*64, out_channels=64)
+        self.dec1_1 = CBR2d(in_channels=64, out_channels=64)
+
+        ## segmentation 에 필요한 n개의 클래스에 대한 output을 만들기 위해
+        ## 녹색화살표와 같이 1x1 conv 레이어 정의
+
+        self.fc = nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1, stride=1, padding=0, bias=True)
+
+        # 이까지 하면 UNet에서 필요한 레이어들을 모두 초기화 한 것
+
+    ## 이번엔 각 레이어들을 연결
+    # x는 인풋 이미지
+    def forward(self, x):
+        # 첫번째 스테이지의 첫번째 레이어를 연결
+        enc1_1 = self.enc1_1(x)
+        enc1_2 = self.enc1_2(enc1_1)
+        pool1 = self.pool1(enc1_2)
+
+        enc2_1 = self.enc2_1(pool1)
+        enc2_2 = self.enc2_2(enc2_1)
+        pool2 = self.pool2(enc2_2)
+
+        enc3_1 = self.enc3_1(pool2)
+        enc3_2 = self.enc3_2(enc3_1)
+        pool3 = self.pool3(enc3_2)
+
+        enc4_1 = self.enc4_1(pool3)
+        enc4_2 = self.enc4_2(enc4_1)
+        pool4 = self.pool4(enc4_2)
+
+        enc5_1 = self.enc5_1(pool4)
+
+        # 이까지 하면 엔코더 스테이지 다 연결
+
+        # 다음은 디코더를 연결
+        dec5_1 = self.dec5_1(enc5_1)
+
+        unpool4 = self.unpool4(dec5_1)
+        ## 채널 방향으로 연결하는 함수를 concatination, cat 이라고 함
+        ## dim: 1 -> 채널방향, 0 -> batch 방향, 2 -> y 방향, 3 -> x 방향
+        cat4 = torch.cat((unpool4, enc4_2), dim=1)
+        dec4_2 = self.dec4_2(cat4)
+        dec4_1 = self.dec4_1(dec4_2)
+
+        unpool3 = self.unpool3(dec4_1)
+        cat3 = torch.cat((unpool3, enc3_2), dim=1)
+        dec3_2 = self.dec3_2(cat3)
+        dec3_1 = self.dec3_1(dec3_2)
+
+        unpool2 = self.unpool2(dec3_1)
+        cat2 = torch.cat((unpool2, enc2_2), dim=1)
+        dec2_2 = self.dec2_2(cat2)
+        dec2_1 = self.dec2_1(dec2_2)
+
+        unpool1 = self.unpool1(dec2_1)
+        cat1 = torch.cat((unpool1, enc1_2), dim=1)
+        dec1_2 = self.dec1_2(cat1)
+        dec1_1 = self.dec1_1(dec1_2)
+
+        x = self.fc(dec1_1)
+
+        return x
