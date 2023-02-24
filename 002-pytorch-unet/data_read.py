a047f07df2cee598a871e5b7219d1db6c4f8cc2e
Ga-ng <kounghun0328@gmail.com>
2023-02-24 11:52:38 +0900

    UNet: data preprocessing

---
+++ 002-pytorch-unet/data_read.py
@@ -0,0 +1,98 @@
+## 30개의 프레임으로 이뤄진 tif 파일을 하나의 프레임 씩 나눠
+## 따로따로 저장할 수 있도록 코드를 작성할 것
+
+import os
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+## 데이터 불러오기
+dir_data = './datasets'
+
+name_label = 'train-labels.tif'
+name_input = 'train-volume.tif'
+
+img_label = Image.open(os.path.join(dir_data, name_label))
+img_input = Image.open(os.path.join(dir_data, name_input))
+
+ny, ny = img_label.size
+nframe = img_label.n_frames
+
+## Traing, Vaildation, Test set을 나누어 져장하기
+## 30 frame 중 24개는 train 3개는 vaild 3개는 test로 사용할 것
+
+nframe_train = 24
+nframe_val = 3
+nframe_test = 3
+
+## 데이터가 저장된 디렉토리 설정
+dir_save_train = os.path.join(dir_data, 'train')
+dir_save_val = os.path.join(dir_data, 'val')
+dir_save_test = os.path.join(dir_data, 'test')
+
+## 디렉토리 생성
+if not os.path.exists(dir_save_train):
+    os.makedirs(dir_save_train)
+
+if not os.path.exists(dir_save_val):
+    os.makedirs(dir_save_val)
+
+if not os.path.exists(dir_save_test):
+    os.makedirs(dir_save_test)
+
+## 실제로 데이터를 나눠서 저장
+## 랜덤하게 저장하기 위해 frame에 대해 랜덤한 index 생성
+id_frame = np.arange(nframe)
+np.random.shuffle(id_frame)
+
+## 실세로 데이터셋을 저장하는 구문을 작성
+## 1. training set
+offset_nframe = 0
+
+for i in range(nframe_train):
+    img_label.seek(id_frame[i + offset_nframe])
+    img_input.seek(id_frame[i + offset_nframe])
+
+    label_ = np.asarray(img_label)
+    input_ = np.asarray(img_input)
+
+    np.save(os.path.join(dir_save_train, 'label_%03d.npy' % i), label_)
+    np.save(os.path.join(dir_save_train, 'input_%03d.npy' % i), input_)
+
+## 2. validation set
+offset_nframe += nframe_train
+
+for i in range(nframe_val):
+    img_label.seek(id_frame[i + offset_nframe])
+    img_input.seek(id_frame[i + offset_nframe])
+
+    label_ = np.asarray(img_label)
+    input_ = np.asarray(img_input)
+
+    np.save(os.path.join(dir_save_val, 'label_%03d.npy' % i), label_)
+    np.save(os.path.join(dir_save_val, 'input_%03d.npy' % i), input_)
+
+## 3. test set 저장
+offset_nframe += nframe_val
+
+for i in range(nframe_test):
+    img_label.seek(id_frame[i + offset_nframe])
+    img_input.seek(id_frame[i + offset_nframe])
+
+    label_ = np.asarray(img_label)
+    input_ = np.asarray(img_input)
+
+    np.save(os.path.join(dir_save_test, 'label_%03d.npy' % i), label_)
+    np.save(os.path.join(dir_save_test, 'input_%03d.npy' % i), input_)
+
+
+## 생성한 데이터 셋 출력
+plt.subplot(121)
+plt.imshow(label_, cmap='gray')
+plt.title('label')
+
+plt.subplot(122)
+plt.imshow(input_, cmap='gray')
+plt.title('input')
+
+plt.show()
